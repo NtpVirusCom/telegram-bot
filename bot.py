f43525fea455724979826bb684861800cea328a0
ntpviruscom <ntpviruscom@gmail.com>
2026-01-28 11:40:38 +0700

    initial telegram bot

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+#BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = "8465996340:AAHLEqlMqcjIbWDCcM453h6L2CYpOAlavQ8"
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot is running on Railway!")
+
+def main():
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Portfolio Bot Started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
