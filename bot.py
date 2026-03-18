3a7d607561d19db83e40c20563c71bab477f4426
NtpVirusCom <105095675+NtpVirusCom@users.noreply.github.com>
2026-03-19 01:53:44 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1898,12 +1898,12 @@ async def cmd_start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def menu_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
 
-    #await query.answer()
+    await query.answer()
 
-    try:
-        await query.answer()
-    except BadRequest:
-        pass
+    #try:
+    #    await query.answer()
+    #except BadRequest:
+    #    pass
 
     data = query.data
 
